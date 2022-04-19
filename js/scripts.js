d70e4360fbb3a51534aa4d673b3dfdefb97cb456
carnivalgoblin <rico.prodan@gmail.com>
2022-04-19 12:46:34 +0200

    optimized array creation and added a for and if-else loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 let pokemonList = [];
 
-pokemonList[0] = {name: "Bulbasaur", height: 7, types: ['grass','poison']};
-pokemonList[1] = {name: "Pikachu", height: 2, types: ['electric','speed']};
-pokemonList[2] = {name: "Charizard", height: 15, types: ['fire','flying']};
+pokemonList = [
+{name: "Bulbasaur", height: 7, types: ['grass','poison']},
+{name: "Pikachu", height: 2, types: ['electric','speed']},
+{name: "Charizard", height: 15, types: ['fire','flying']}
+];
+
+for (let i=0; i<3; i++) {
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")")
+    if (pokemonList[i].height > 5) {document.write(" - wow this pokemon is big!"+"<br>")
+}else [document.write("<br>")]
+}
