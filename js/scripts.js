83631f937d500e5a41aed762e20badf3c7adeb36
carnivalgoblin <rico.prodan@gmail.com>
2022-04-20 11:53:07 +0200

    added indentation and list to JS file

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,18 @@
 let pokemonList = [];
 
 pokemonList = [
-{name: "Bulbasaur", height: 7, types: ['grass','poison']},
-{name: "Pikachu", height: 2, types: ['electric','speed']},
-{name: "Charizard", height: 15, types: ['fire','flying']}
+    { name: "Bulbasaur", height: 7, types: ['grass', 'poison'] },
+    { name: "Pikachu", height: 2, types: ['electric', 'speed'] },
+    { name: "Charizard", height: 15, types: ['fire', 'flying'] }
 ];
 
-for (let i=0; i<3; i++) {
-    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")")
-    if (pokemonList[i].height > 5) {document.write(" - wow this pokemon is big!"+"<br>")
-}else [document.write("<br>")]
+document.write('<ul>')
+
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write('<li>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')')
+    if (pokemonList[i].height > 5) {
+        document.write(' - wow this pokemon is big!' + '</li>' + '<br>')
+    } else { document.write('</li>' + '<br>') }
 }
+
+document.write('</ul>')
