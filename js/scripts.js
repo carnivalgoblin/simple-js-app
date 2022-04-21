5dbab053027557ec56387d7fdc9fe7ba4c0c902f
carnivalgoblin <rico.prodan@gmail.com>
2022-04-21 14:37:00 +0200

    added IIFE around pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,22 @@
-let pokemonList = [];
+let pokemonRepository = (function () {
+    let pokemonList = [];
 
-pokemonList = [
-    { name: "Bulbasaur", height: 7, types: ['grass', 'poison'] },
-    { name: "Pikachu", height: 2, types: ['electric', 'speed'] },
-    { name: "Charizard", height: 15, types: ['fire', 'flying'] }
-];
+    pokemonList = [
+        { name: "Bulbasaur", height: 7, types: ['grass', 'poison'] },
+        { name: "Pikachu", height: 2, types: ['electric', 'speed'] },
+        { name: "Charizard", height: 15, types: ['fire', 'flying'] }
+    ];
+
+    return {
+        add: function (item) {
+            pokemonList.push(item);
+        },
+        getAll: function () {
+            return pokemonList;
+        }
+
+    }
+})();
 
 /* document.write('<ul class="pokemon-list">')
 
@@ -19,7 +31,7 @@ document.write('</ul>') */
 
 document.write('<ul class="pokemon-list">')
 
-pokemonList.forEach(function (pokemon) {
+pokemonRepository.getAll().forEach(function (pokemon) {
     document.write('<li>' + pokemon.name + ' (height: ' + pokemon.height + ')')
     if (pokemon.height > 5) {
         document.write(' - wow this pokemon is big!' + '</li>' + '<br>')
