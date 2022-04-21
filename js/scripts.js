62372ecab6662c492c10c4db7132af2ac98b73d4
carnivalgoblin <rico.prodan@gmail.com>
2022-04-21 14:29:34 +0200

    added forEach Loop

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ pokemonList = [
     { name: "Charizard", height: 15, types: ['fire', 'flying'] }
 ];
 
-document.write('<ul class="pokemon-list">')
+/* document.write('<ul class="pokemon-list">')
 
 for (let i = 0; i < pokemonList.length; i++) {
     document.write('<li>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')')
@@ -14,5 +14,16 @@ for (let i = 0; i < pokemonList.length; i++) {
         document.write(' - wow this pokemon is big!' + '</li>' + '<br>')
     } else { document.write('</li>' + '<br>') }
 }
+
+document.write('</ul>') */
+
+document.write('<ul class="pokemon-list">')
+
+pokemonList.forEach(function (pokemon) {
+    document.write('<li>' + pokemon.name + ' (height: ' + pokemon.height + ')')
+    if (pokemon.height > 5) {
+        document.write(' - wow this pokemon is big!' + '</li>' + '<br>')
+    } else { document.write('</li>' + '<br>') }
+});
 
 document.write('</ul>')
