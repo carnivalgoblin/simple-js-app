e039dc780d08d348ec70b6303813dbf96ebbff8c
carnivalgoblin <rico.prodan@gmail.com>
2022-04-25 07:26:38 +0200

    remove unecessary List and commented adding pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -44,7 +44,7 @@ document.write('</ul> <br>');
 
 /* DEFINE NEW POKEMON, WRITE TO DOM AND ADD IT TO REPO*/
 
-let newPokemon = { name: 'Ditto', height: 0.3, types: ['normal'] };
+/* let newPokemon = { name: 'Ditto', height: 0.3, types: ['normal'] };
 
 
 document.write('<ul class="pokemon-list">');
@@ -58,47 +58,4 @@ if (newPokemon.height > 1) {
 
 document.write('</ul> <br><br>');
 
-pokemonRepository.add(newPokemon);
-
-
-/* CALL NEW LIST OF POKEMON */
-
-document.write('<h1>New List of Pokemon:</h1> <br>');
-
-document.write('<ul class="pokemon-list">');
-
-pokemonRepository.getAll().forEach(function (pokemon) {
-    document.write('<li>' + pokemon.name + ' (height: ' + pokemon.height + ')')
-    if (pokemon.height > 1) {
-        document.write(' - wow this pokemon is big!' + '</li>' + '<br>')
-    } else { document.write('</li>' + '<br><br>') }
-});
-
-/* setTimeout(() => { document.write('Hello World!') }, 1000); */
-
-/* ADD INVALID POKEMON */
-
-let invPokemon = 'Psyduck'; /* STRING */
-
-document.write('<ul class="pokemon-list">');
-document.write('Adding new Pokemon:<br><br>');
-document.write('<li>' + invPokemon + '</li>' + '<br>');
-document.write('</ul> <br><br>');
-
-pokemonRepository.add(invPokemon);
-
-document.write('<p>' + 'Neccesary keys of object are: ' + Object.keys(pokemonRepository.getAll()['0']) + '</p>');
-
-/* FILTER */
-
-let searchPokemon = 'Pikachu';
-
-function isPresent(namePokemon) {
-    for (let i = 0; i < pokemonRepository.getAll().length; i++) {
-        if (Object.values(pokemonRepository.getAll()[i])[0] === namePokemon) {
-            return namePokemon + ' is present in list.';
-        }
-    }
-};
-
-console.log(isPresent(searchPokemon));
+pokemonRepository.add(newPokemon); */
